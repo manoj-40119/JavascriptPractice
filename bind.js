70e93b52fa99dce20e7bcfa35aab73474b94a2c1
manoj-40119 <manojkumarbonu1999@gmail.com>
2021-09-16 19:04:12 +0530

    bind

---
+++ bind.js
@@ -0,0 +1,18 @@
+let cse2021=["c","java","python"];
+let ece2021=["vlsi","cmos","m1"];
+const Student1 = {
+    name:"Manoj",
+    year:"2020",
+    branch:"ECE"
+}
+const Student2 = {
+    name:"Sai",
+    year:"2021",
+    branch:"CSE"
+}
+const college = function(sub,sub1,sub2){
+    console.log(this.name+" "+this.year+" "+this.branch+" "+sub+" "+sub1+" "+sub2);
+
+}
+var final=college.bind(Student2,"c","java","python");
+final();
